f7ea936115839c4d6ec5954dc3ab8ebef8c49e38
Connor Gilroy <ccgilroy@gmail.com>
2020-01-23 16:18:11 -0800

    change fira code to fira mono

--- make.R
+++ make.R
@@ -1,2 +1,5 @@
 rmarkdown::render("index.Rmd", output_file = "r-intro.html")
 rmarkdown::render("index.Rmd", output_format = "all")
+rmarkdown::render("index.Rmd", 
+                  output_format = rmarkdown::pdf_document(), 
+                  output_file = "r-intro.pdf")
